89159361e6a5667c25773afa3bf53230a86c7c3b
Beciuch <bernadettapawlaczyk@dmail.com>
2021-10-12 11:39:34 +0200

    Initial commit

---
+++ src/main/java/pl/nullpointerexception/restapiyt1/RestApiYt1Application.java
@@ -0,0 +1,13 @@
+package pl.nullpointerexception.restapiyt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiYt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiYt1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/nullpointerexception/restapiyt1/RestApiYt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nullpointerexception.restapiyt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiYt1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
